2e41431f88de27a85ee0d59295ea132535842f67
Sterling <sterlingduncan9@gmail.com>
2023-06-14 11:24:56 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Use array inside an object
+
+const adventurer = {
+    name: "Sterling",
+    hitPoint: 10,
+    belongings: ["katana", "potions", "armor"]
+}
+
+// access the values in the array
+
+console.log(adventurer.belongings);
