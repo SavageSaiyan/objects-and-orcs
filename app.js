cafe1ac6a6a60ace40b9dc2c3e4c6701a8ab4a5a
Sterling <sterlingduncan9@gmail.com>
2023-06-14 12:24:28 -0400

    creating customizable oject with classes

--- app.js
+++ app.js
@@ -1,30 +1,128 @@
-// // Use array inside an object
+// // // Use array inside an object
 
-const adventurer = {
-    name: "Sterling",
-    hitPoint: 10,
-    belongings: ["katana", "potions", "armor"],
-    companion: {
-        name: "ace",
-        type: "dog"
+// const adventurer = {
+//     name: "Sterling",
+//     hitPoint: 10,
+//     belongings: ["katana", "potions", "armor"],
+//     companion: {
+//         name: "ace",
+//         type: "dog",
+//         belongings: ["squeaky toy", "healing potion", "health insurance", "satchel of elements"]
+//     }
+// }
+
+// console.log(`My companions name is ${adventurer.companion.name}`);
+// console.log(`One of my companions belongings is ${adventurer.companion.belongings[2]}`)
+
+// // // access the values in the array
+
+// // console.log(adventurer.belongings);
+
+// // // access the first belonging
+
+// // console.log(`first belonging ${adventurer.belongings[0]}`);
+
+// // // iterate over an array inside of an object
+
+// for (let i =0; i < adventurer.belongings.length; i++) {
+//     console.log(adventurer.belongings[i]);
+// }
+
+// // #1 OBJ within OBJ
+
+// const movies = [
+//     {
+//         title: "Tokyo story"
+//     },
+//     {
+//         title: "Paul blart: Mall Cop"
+//     },
+//     {
+//         title: "Sprited Away"
+//     }
+// ];
+
+// console.log(movies[0].title) // object
+
+// // Looping over array of objects
+
+// for(let i =0; i < movies.length; i++) {
+//     console.log(movies[i].title);
+// }
+
+// Combine objects, arrays, and functions
+
+// const foo = {
+//     arr: [1,2,3],
+//     obj: {
+//         prop: "object property"
+//     },
+//     doSomething: () => {
+//         cdonsole.log("I did something!")
+//     }
+// }
+
+// console.log(foo.arr[0]); // 1
+// console.log(foo.obj.prop) // object property
+// foo.doSomething();
+
+
+// AN array of arrays
+
+// const foo = [
+//     [1,2,3],
+//     ["4","5","6"],
+//     [7,8,9],
+//     ()=> {
+//         console.log("I am a function inside an array")
+//     }
+// ]
+
+// console.log(foo[0]) // []
+// console.log(foo[0][1]) // 2
+
+// // Storing functions in an array
+// console.log(foo[3]{})
+
+
+// Creating A Class
+
+class Character {
+    constructor (name, age, eyes, hair, lovesCats = true, lovesDogs) {
+        this.legs = 2;
+        this.arms = 2;
+        this.name = name;
+        this.age = age;
+        this.eyes = eyes;
+        this.hair = hair;
+        this.lovesCats = lovesCats;
+        this.lovesDogs = lovesDogs || true;
     }
+
+    setHairColor(HairColor) {
+            this.hair = HairColor;
+    }
+
+
+    //method1
+   greet (otherCharacter) {
+    console.log(`hello${otherCharacter}`);
+   }
+   //method2
+   smite () {
+        console.log('I smite thee you vile person!')
+   }
 }
 
-console.log(adventurer.companion.name);
+const alexander = new Character();
+const brock = new Character();
 
-// // access the values in the array
+// alexander.greet("Brock ");
+// brock.greet("Alexander");
+// alexander.smite()
 
-// console.log(adventurer.belongings);
-
-// // access the first belonging
-
-// console.log(`first belonging ${adventurer.belongings[0]}`);
-
-// // iterate over an array inside of an object
-
-for (let i =0; i < adventurer.belongings.length; i++) {
-    console.log(adventurer.belongings[i]);
-}
-
-// #1 OBJ within OBJ
-
+// name. age, eyes, hair, cats, dogs
+const azreal = new Character('azreal', '150', 'green', 'silver');
+azreal.eyes = 'brown';
+azreal.setHairColor('red')
+console.log(azreal); 
