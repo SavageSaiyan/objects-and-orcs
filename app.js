1f5e98bb9268c1a851e33a90dda2763774f22dd7
Sterling <sterlingduncan9@gmail.com>
2023-06-14 11:34:31 -0400

    objects within objects

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-// Use array inside an object
+// // Use array inside an object
 
 const adventurer = {
     name: "Sterling",
     hitPoint: 10,
-    belongings: ["katana", "potions", "armor"]
+    belongings: ["katana", "potions", "armor"],
+    companion: {
+        name: "ace",
+        type: "dog"
+    }
 }
 
-// access the values in the array
+console.log(adventurer.companion.name);
 
-console.log(adventurer.belongings);
+// // access the values in the array
+
+// console.log(adventurer.belongings);
+
+// // access the first belonging
+
+// console.log(`first belonging ${adventurer.belongings[0]}`);
+
+// // iterate over an array inside of an object
+
+for (let i =0; i < adventurer.belongings.length; i++) {
+    console.log(adventurer.belongings[i]);
+}
+
+// #1 OBJ within OBJ
+
